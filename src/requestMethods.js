3bc88431acc829fd82e2f00b88fc4f8c4fdd6fd0
Antariksh Alse <alse.antariksh@mahindra.com>
2023-08-31 11:41:33 +0530

    Update : API Url

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const BASE_URL = "http://localhost:5000/api/";
-const BASE_URL = "https://17xhzu.deta.dev/api/";
+const BASE_URL = "https://ecomapi-1-m3999497.deta.app/";
 const tokenobj = localStorage.getItem("persist:root");
 const TokenContainer = tokenobj ? JSON.parse(tokenobj).currentUser : "";
 const TOKEN = TokenContainer ? JSON.parse(TokenContainer).accessToken : "";
